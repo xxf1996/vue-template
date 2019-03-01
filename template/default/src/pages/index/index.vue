f7a7cb6880eaf102726cfe25f83cbec4d41ba4dc
xiexuefeng <1158231926@qq.com>
2019-03-01 17:41:13 +0800

    change structure

---
+++ template/default/src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <d2-container class="index">
+    <d2-page-cover>
+      <p class="title">基于D2Admin的中后台模板</p>
+    </d2-page-cover>
+  </d2-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  margin-top: 100px;
+  font-size: 24px;
+  text-align: center;
+}
+</style>
